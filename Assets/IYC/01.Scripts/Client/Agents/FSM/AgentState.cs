6dd104ab737205298137652bc95aaf891eb6fe6f
didrudwns <didrudwns123@gmail.com>
2026-07-08 14:41:54 +0900

    ff

--- Assets/IYC/01.Scripts/Client/Agents/FSM/AgentState.cs
+++ Assets/IYC/01.Scripts/Client/Agents/FSM/AgentState.cs
@@ -16,7 +16,7 @@
 
         public virtual void Enter(float transitionDuration, int layerIndex = 0)
         {
-            // _renderer.PlayClip(_stateClipHash, 0f, transitionDuration, layerIndex);
+            _renderer.PlayClip(_stateClipHash, 0f, transitionDuration, layerIndex);
         }
 
         public virtual void Update() { }
