3016889e02ffcbe15942954af9d66536ebb690a3
riwoo0902 <imliu4342@gmail.com>
2026-07-08 14:14:33 +0900

    save

---
+++ Assets/Lrw/Script/Spine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Lrw.Script
+{
+    public class Spine : MonoBehaviour
+    {
+        [SerializeField] private float speed = 5;
+
+        private void FixedUpdate()
+        {
+            transform.Rotate(Vector3.up, speed * Time.fixedDeltaTime);
+        }
+    }
+}
